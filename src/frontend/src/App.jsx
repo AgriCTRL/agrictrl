0ccb71b64e825a2bafc0a7e00904d3d74a21a00e
AgriCTRL <agrictrl@gmail.com>
2024-07-17 20:58:52 +0800

    Add files via upload

---
+++ src/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import Register from "./Register";
+
+function App() {
+
+  return (
+    <main className="font-kanit">
+      {/* <div className="flex flex-col items-center">
+        <img src="/logo2.svg" alt="DFINITY logo" />
+        <p className="text-4xl mt-10">Welcome to Internet Computer!</p>
+        <p className="text-2xl mt-5">Build your dapps using this template, have fun!</p>
+      </div> */}
+      <Register/>
+    </main>
+  );
+}
+
+export default App;
