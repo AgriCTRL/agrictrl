854c93d44e81cfc32a191e417b736c2b3a53b884
Jobert Mampusti <mampustijobert@gmail.com>
2024-07-26 09:42:18 +0800

    dashboard backend integration

--- frontend/src/Pages/Trader/Dashboard.jsx
+++ frontend/src/Pages/Trader/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import UserLayout from '@/Layouts/UserLayout';
 import CardComponent from '@/Components/CardComponent';
 import { Divider } from 'primereact/divider';
@@ -9,6 +9,87 @@ import {
     Building2
 } from "lucide-react";
 function Dashboard() {
+    const [riceBatchesCount, setRiceBatchesCount] = useState(0);
+    const [tradersCount, setTradersCount] = useState(0);
+    const [warehousesCount, setWarehousesCount] = useState(0);
+    const [dryersCount, setDryersCount] = useState(0);
+    const [millersCount, setMillersCount] = useState(0);
+
+    const facilitiesCount = warehousesCount + dryersCount + millersCount;
+    
+    useEffect(() => {
+        const fetchRiceBatchesCount = async () => {
+            try {
+                const res = await fetch('http://bkyz2-fmaaa-aaaaa-qaaaq-cai.localhost:4943/ricebatches/count', {
+                    method: 'GET',
+                    headers: { 'Content-Type': 'application/json'}
+                });
+                const data = await res.json();
+                setRiceBatchesCount(data);
+            }
+            catch (error) {
+                console.log(error.message);
+            }
+        };
+        const fetchTradersCount = async () => {
+            try {
+                const res = await fetch('http://bkyz2-fmaaa-aaaaa-qaaaq-cai.localhost:4943/nfapersonnels/count', {
+                    method: 'GET',
+                    headers: { 'Content-Type': 'application/json'}
+                });
+                const data = await res.json();
+                setTradersCount(data);
+            }
+            catch (error) {
+                console.log(error.message);
+            }
+        };
+        const fetchWarehousesCount = async () => {
+            try {
+                const res = await fetch('http://bkyz2-fmaaa-aaaaa-qaaaq-cai.localhost:4943/warehouses/count', {
+                    method: 'GET',
+                    headers: { 'Content-Type': 'application/json'}
+                });
+                const data = await res.json();
+                setWarehousesCount(data);
+            }
+            catch (error) {
+                console.log(error.message);
+            }
+        };
+        const fetchDryersCount = async () => {
+            try {
+                const res = await fetch('http://bkyz2-fmaaa-aaaaa-qaaaq-cai.localhost:4943/dryers/count', {
+                    method: 'GET',
+                    headers: { 'Content-Type': 'application/json'}
+                });
+                const data = await res.json();
+                setDryersCount(data);
+            }
+            catch (error) {
+                console.log(error.message);
+            }
+        };
+        const fetchMillersCount = async () => {
+            try {
+                const res = await fetch('http://bkyz2-fmaaa-aaaaa-qaaaq-cai.localhost:4943/millers/count', {
+                    method: 'GET',
+                    headers: { 'Content-Type': 'application/json'}
+                });
+                const data = await res.json();
+                setMillersCount(data);
+            }
+            catch (error) {
+                console.log(error.message);
+            }
+        };
+        fetchRiceBatchesCount();
+        fetchTradersCount();
+        fetchWarehousesCount();
+        fetchDryersCount();
+        fetchMillersCount();
+    }, [])
+
     return (
         <UserLayout activePage="Dashboard">
             <CardComponent>
@@ -17,7 +98,7 @@ function Dashboard() {
                         <Wheat />
                         <p className='font-bold'>Rice Tracked</p>
                     </div>
-                    <h1 className='text-heading text-primary text-center font-bold'>0</h1>
+                    <h1 className='text-heading text-primary text-center font-bold'>{riceBatchesCount}</h1>
                 </CardComponent>
                 <Divider className='bg-lightest-grey w-px my-4' layout='vertical'/>
                 <CardComponent className='flex-1 flex-col gap-4 justify-center'>
@@ -25,7 +106,7 @@ function Dashboard() {
                         <HeartHandshake />
                         <p className='font-bold'>Traders</p>
                     </div>
-                    <h1 className='text-heading text-primary text-center font-bold'>0</h1>
+                    <h1 className='text-heading text-primary text-center font-bold'>{tradersCount}</h1>
                 </CardComponent>
                 <Divider className='bg-lightest-grey w-px my-4' layout='vertical'/>
                 <CardComponent className='flex-1 flex-col gap-4 justify-center'>
@@ -41,7 +122,7 @@ function Dashboard() {
                         <Building2 />
                         <p className='font-bold'>Facilities</p>
                     </div>
-                    <h1 className='text-heading text-primary text-center font-bold'>0</h1>
+                    <h1 className='text-heading text-primary text-center font-bold'>{facilitiesCount}</h1>
                 </CardComponent>
             </CardComponent>
         </UserLayout>
