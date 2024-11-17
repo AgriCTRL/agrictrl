e4d5e187df0baf212c871613541af8a51879e9bc
jrlbln <joerelbelen02@gmail.com>
2024-11-17 19:28:03 +0800

    added additional fields && limit offset

--- frontend/src/Pages/SMS/Staff/Procurement/PalayRegisterForm/LogisticsInfoForm.jsx
+++ frontend/src/Pages/SMS/Staff/Procurement/PalayRegisterForm/LogisticsInfoForm.jsx
@@ -1,64 +1,88 @@
-import React from 'react';
-import { InputText } from 'primereact/inputtext';
-import { InputTextarea } from 'primereact/inputtextarea';
-import { Dropdown } from 'primereact/dropdown';
+import React from "react";
+import { InputText } from "primereact/inputtext";
+import { Dropdown } from "primereact/dropdown";
 
-const LogisticsInfoForm = ({ 
-    palayData, 
-    transactionData, 
-    handlePalayInputChange, 
-    handleTransactionInputChange,
-    handleLocationId,
-    locationOptions, 
-    errors 
-  }) => {
-    return (
-        <div className="flex flex-col h-full w-full gap-2">
-            <div className="flex w-full gap-4">
-                <div className="w-full">
-                    <label htmlFor="buyingStationName" className="block text-sm font-medium text-gray-700 mb-1">Bought at</label>
-                    <InputText
-                        id="buyingStationName"
-                        name="buyingStationName"
-                        value={palayData.buyingStationName}
-                        onChange={handlePalayInputChange}
-                        placeholder="Enter buying station name"
-                        className="ring-0 w-full placeholder:text-gray-400"
-                        maxLength={50}
-                    />
-                    {errors.buyingStationName && <p className="text-red-500 text-xs mt-1">{errors.buyingStationName}</p>}
-                </div>
+const LogisticsInfoForm = ({
+  palayData,
+  transactionData,
+  handlePalayInputChange,
+  handleTransactionInputChange,
+  handleLocationId,
+  locationOptions,
+  errors,
+}) => {
+  return (
+    <div className="flex flex-col h-full w-full gap-4">
+      <div className="flex w-full gap-4">
+        <div className="w-full">
+          <label
+            htmlFor="buyingStationName"
+            className="block text-sm font-medium text-gray-700 mb-1"
+          >
+            Bought at
+          </label>
+          <InputText
+            id="buyingStationName"
+            name="buyingStationName"
+            value={palayData.buyingStationName}
+            onChange={handlePalayInputChange}
+            placeholder="Enter buying station name"
+            className="ring-0 w-full placeholder:text-gray-400"
+            maxLength={50}
+          />
+          {errors.buyingStationName && (
+            <p className="text-red-500 text-xs mt-1">
+              {errors.buyingStationName}
+            </p>
+          )}
+        </div>
 
-                <div className="w-full">
-                    <label htmlFor="buyingStationLoc" className="block text-sm font-medium text-gray-700 mb-1">Location</label>
-                    <InputText
-                        id="buyingStationLoc"
-                        name="buyingStationLoc"
-                        value={palayData.buyingStationLoc}
-                        onChange={handlePalayInputChange}
-                        placeholder="Enter buying station location"
-                        className='w-full focus:ring-0'
-                        maxLength={50}
-                    />
-                    {errors.buyingStationLoc && <p className="text-red-500 text-xs mt-1">{errors.buyingStationLoc}</p>}
-                </div>
-            </div>
-            
-            <div className="w-full">
-                <label htmlFor="transporterName" className="block text-sm font-medium text-gray-700 mb-1">Transported by</label>
-                <InputText
-                    id="transporterName"
-                    name="transporterName"
-                    value={transactionData.transporterName}
-                    onChange={handleTransactionInputChange}  
-                    placeholder="Enter transport"
-                    className='w-full focus:ring-0'
-                    maxLength={50}
-                />
-                {errors.transporterName && <p className="text-red-500 text-xs mt-1">{errors.transporterName}</p>}
-            </div>
+        <div className="w-full">
+          <label
+            htmlFor="buyingStationLoc"
+            className="block text-sm font-medium text-gray-700 mb-1"
+          >
+            Location
+          </label>
+          <InputText
+            id="buyingStationLoc"
+            name="buyingStationLoc"
+            value={palayData.buyingStationLoc}
+            onChange={handlePalayInputChange}
+            placeholder="Enter buying station location"
+            className="w-full focus:ring-0"
+            maxLength={50}
+          />
+          {errors.buyingStationLoc && (
+            <p className="text-red-500 text-xs mt-1">
+              {errors.buyingStationLoc}
+            </p>
+          )}
+        </div>
+      </div>
 
-            <div className="w-full">
+      <div className="w-full">
+        <label
+          htmlFor="transporterName"
+          className="block text-sm font-medium text-gray-700 mb-1"
+        >
+          Transported by
+        </label>
+        <InputText
+          id="transporterName"
+          name="transporterName"
+          value={transactionData.transporterName}
+          onChange={handleTransactionInputChange}
+          placeholder="Enter transport"
+          className="w-full focus:ring-0"
+          maxLength={50}
+        />
+        {errors.transporterName && (
+          <p className="text-red-500 text-xs mt-1">{errors.transporterName}</p>
+        )}
+      </div>
+
+      {/* <div className="w-full">
                 <label htmlFor="transporterDesc" className="block text-sm font-medium text-gray-700 mb-1">Transport Description</label>
                 <InputTextarea
                     id="transporterDesc"
@@ -70,53 +94,134 @@ const LogisticsInfoForm = ({
                     maxLength={250}
                 />
                 {errors.transporterDesc && <p className="text-red-500 text-xs mt-1">{errors.transporterDesc}</p>}
-            </div>
+            </div> */}
 
-            <div className="w-full">
-                <label htmlFor="sendToWarehouse" className="block text-sm font-medium text-gray-700 mb-1">Send to</label>
-                <div className="flex flex-row w-full space-x-2">
-                    <div className="flex flex-col w-full">
-                        <InputText
-                            id="sendToWarehouse"
-                            name="sendToWarehouse"
-                            value={transactionData.toLocationType}
-                            placeholder="Select location"
-                            className="ring-0 w-full placeholder:text-gray-400"
-                            disabled
-                            maxLength={50}
-                        />
-                        {errors.toLocationType && <p className="text-red-500 text-xs mt-1">{errors.toLocationType}</p>}
-                    </div>
-                    <div className="flex flex-col w-full">
-                        <Dropdown
-                            id="facility"
-                            name="facility"
-                            value={transactionData.toLocationId}
-                            options={locationOptions}
-                            onChange={handleLocationId}
-                            placeholder="Select facility"
-                            className="ring-0 w-full placeholder:text-gray-400"
-                        />
-                        {errors.toLocationId && <p className="text-red-500 text-xs mt-1">{errors.toLocationId}</p>}
-                    </div>
-                </div>
-            </div>
-
-            <div className="w-full">
-                <label htmlFor="remarks" className="block text-sm font-medium text-gray-700 mb-1">Remarks</label>
-                <InputTextarea
-                    id="remarks"
-                    name="remarks"
-                    value={transactionData.remarks}
-                    onChange={handleTransactionInputChange} 
-                    placeholder="Enter Remarks"
-                    className="w-full ring-0"
-                    maxLength={250}
-                />
-                {errors.remarks && <p className="text-red-500 text-xs mt-1">{errors.remarks}</p>}
-            </div>
+      <div className="w-full">
+        <label
+          htmlFor="sendToWarehouse"
+          className="block text-sm font-medium text-gray-700 mb-1"
+        >
+          Send to
+        </label>
+        <div className="flex flex-row w-full space-x-2">
+          <div className="flex flex-col w-full">
+            <InputText
+              id="sendToWarehouse"
+              name="sendToWarehouse"
+              value={transactionData.toLocationType}
+              placeholder="Select location"
+              className="ring-0 w-full placeholder:text-gray-400"
+              disabled
+              maxLength={50}
+            />
+            {errors.toLocationType && (
+              <p className="text-red-500 text-xs mt-1">
+                {errors.toLocationType}
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col w-full">
+            <Dropdown
+              id="facility"
+              name="facility"
+              value={transactionData.toLocationId}
+              options={locationOptions}
+              onChange={handleLocationId}
+              placeholder="Select facility"
+              className="ring-0 w-full placeholder:text-gray-400"
+            />
+            {errors.toLocationId && (
+              <p className="text-red-500 text-xs mt-1">{errors.toLocationId}</p>
+            )}
+          </div>
         </div>
-    );
+      </div>
+
+      <div className="flex w-full gap-4">
+        <div className="w-full">
+          <label
+            htmlFor="weighedBy"
+            className="block text-sm font-medium text-gray-700 mb-1"
+          >
+            Weighed By
+          </label>
+          <InputText
+            id="weighedBy"
+            name="weighedBy"
+            value={palayData.weighedBy}
+            onChange={handlePalayInputChange}
+            className="w-full ring-0"
+            maxLength={250}
+          />
+          {errors.weighedBy && (
+            <p className="text-red-500 text-xs mt-1">{errors.weighedBy}</p>
+          )}
+        </div>
+
+        <div className="w-full">
+          <label
+            htmlFor="correctedBy"
+            className="block text-sm font-medium text-gray-700 mb-1"
+          >
+            Certified Correct By
+          </label>
+          <InputText
+            id="correctedBy"
+            name="correctedBy"
+            value={palayData.correctedBy}
+            onChange={handlePalayInputChange}
+            className="w-full ring-0"
+            maxLength={250}
+          />
+          {errors.correctedBy && (
+            <p className="text-red-500 text-xs mt-1">{errors.correctedBy}</p>
+          )}
+        </div>
+      </div>
+
+      <div className="flex w-full gap-4">
+        <div className="w-full">
+          <label
+            htmlFor="classifier"
+            className="block text-sm font-medium text-gray-700 mb-1"
+          >
+            Classified By
+          </label>
+          <InputText
+            id="classifier"
+            name="classifier"
+            value={palayData.classifier}
+            onChange={handlePalayInputChange}
+            className="w-full ring-0"
+            maxLength={250}
+          />
+          {errors.classifier && (
+            <p className="text-red-500 text-xs mt-1">{errors.classifier}</p>
+          )}
+        </div>
+
+        <div className="w-full">
+          <label
+            htmlFor="remarks"
+            className="block text-sm font-medium text-gray-700 mb-1"
+          >
+            Remarks
+          </label>
+          <InputText
+            id="remarks"
+            name="remarks"
+            value={transactionData.remarks}
+            onChange={handleTransactionInputChange}
+            className="w-full ring-0"
+            maxLength={250}
+          />
+          {errors.remarks && (
+            <p className="text-red-500 text-xs mt-1">{errors.remarks}</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export { LogisticsInfoForm };
--- frontend/src/Pages/SMS/Staff/Procurement/Procurement.jsx
+++ frontend/src/Pages/SMS/Staff/Procurement/Procurement.jsx
@@ -8,7 +8,6 @@ import { InputText } from "primereact/inputtext";
 import { Toast } from "primereact/toast";
 import { IconField } from "primereact/iconfield";
 import { InputIcon } from "primereact/inputicon";
-import { Dialog } from "primereact/dialog";
 
 import {
   Search,
@@ -21,6 +20,7 @@ import {
 } from "lucide-react";
 
 import PalayRegister from "./PalayRegister";
+import PalayDetails from "./PalayDetails";
 import { useAuth } from "../../../Authentication/Login/AuthContext";
 import StaffLayout from "@/Layouts/StaffLayout";
 
@@ -34,6 +34,8 @@ function BuyPalay() {
   const [palayCount, setPalayCount] = useState(0);
   const [processedCount, setProcessedCount] = useState(0);
   const [distributedCount, setDistributedCount] = useState(0);
+
+  const [first, setFirst] = useState(0);
 
   const [globalFilterValue, setGlobalFilterValue] = useState("");
   const [showDetails, setShowDetails] = useState(false);
@@ -57,20 +59,33 @@ function BuyPalay() {
     setFilters(newFilters);
   }, [globalFilterValue]);
 
+  useEffect(() => {
+    fetchPalayData(Math.floor(first / 10), globalFilterValue);
+  }, [first, globalFilterValue]);
+
   const onGlobalFilterChange = (e) => {
     setGlobalFilterValue(e.target.value);
   };
 
-  const fetchPalayData = async () => {
+  const fetchPalayData = async (page = 0, searchValue = "") => {
     try {
-      const response = await fetch(`${apiUrl}/palaybatches`);
+      const limit = 10;
+      const offset = page * limit;
 
+      let url = `${apiUrl}/palaybatches?limit=${limit}&offset=${offset}`;
+
+      if (searchValue) {
+        url = `${apiUrl}/palaybatches/search?id=${searchValue}&limit=${limit}&offset=${offset}`;
+      }
+
+      const response = await fetch(url);
       if (!response.ok) {
         throw new Error("Failed to fetch palay data");
       }
 
-      const data = await response.json();
+      const { data, total } = await response.json();
       setInventoryData(data);
+      setPalayCount(total);
     } catch (error) {
       console.error("Error:", error);
       toast.current.show({
@@ -128,6 +143,7 @@ function BuyPalay() {
   };
 
   const handleItemClick = (item) => {
+    console.log(inventoryData)
     setSelectedPalay(item);
     setShowDetails(true);
   };
@@ -175,7 +191,7 @@ function BuyPalay() {
       <div
         className="flex items-center p-4 cursor-pointer bg-gray-100 hover:bg-gray-200  rounded-lg mb-4"
         onClick={() => handleItemClick(item)}
-      > 
+      >
         <div className="flex-none mr-4">
           <Wheat size={40} className="text-gray-400" />
         </div>
@@ -251,17 +267,21 @@ function BuyPalay() {
             </div>
 
             {/* Container with relative positioning */}
-            <div className="relative flex flex-col" style={{ height: "calc(100vh - 430px)" }}>
+            <div
+              className="relative flex flex-col"
+              style={{ height: "calc(100vh - 430px)" }}
+            >
               <DataView
                 value={inventoryData}
                 itemTemplate={itemTemplate}
-                filters={filters}
-                globalFilterFields={["id", "status"]}
-                emptyMessage="No inventory found."
+                lazy
                 paginator
                 rows={10}
-                className="overflow-y-auto pb-16" // Add padding at bottom to prevent content being hidden behind paginator
-                paginatorClassName="absolute bottom-0 left-0 right-0 bg-white border-t" // Fix paginator at bottom
+                first={first}
+                onPage={(e) => setFirst(e.first)}
+                totalRecords={palayCount}
+                className="overflow-y-auto pb-16"
+                paginatorClassName="absolute bottom-0 left-0 right-0 bg-white border-t"
                 paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink"
               />
             </div>
@@ -275,70 +295,11 @@ function BuyPalay() {
         onPalayRegistered={handlePalayRegistered}
       />
 
-      <Dialog
+      <PalayDetails
         visible={showDetails}
         onHide={() => setShowDetails(false)}
-        header={`Batch #${selectedPalay?.id} Details`}
-        className="w-full max-w-2xl"
-      >
-        {selectedPalay && (
-          <div className="grid grid-cols-2 gap-4">
-            <div className="col-span-2 border-b pb-2">
-              <h3 className="font-semibold">Basic Information</h3>
-            </div>
-            <div>
-              <p className="text-gray-600">Gross Weight</p>
-              <p>{selectedPalay.grossWeight} Kg</p>
-            </div>
-            <div>
-              <p className="text-gray-600">Net Weight</p>
-              <p>{selectedPalay.netWeight} Kg</p>
-            </div>
-            <div>
-              <p className="text-gray-600">Quality Type</p>
-              <p>{selectedPalay.qualityType}</p>
-            </div>
-            <div>
-              <p className="text-gray-600">Price/Kg</p>
-              <p>{selectedPalay.price}</p>
-            </div>
-
-            <div className="col-span-2 border-b pb-2 mt-4">
-              <h3 className="font-semibold">Quality Specifications</h3>
-            </div>
-            <div>
-              <p className="text-gray-600">Moisture Content</p>
-              <p>{selectedPalay.qualitySpec.moistureContent}%</p>
-            </div>
-            <div>
-              <p className="text-gray-600">Purity</p>
-              <p>{selectedPalay.qualitySpec.purity}%</p>
-            </div>
-            <div>
-              <p className="text-gray-600">Damage</p>
-              <p>{selectedPalay.qualitySpec.damaged}%</p>
-            </div>
-
-            <div className="col-span-2 border-b pb-2 mt-4">
-              <h3 className="font-semibold">Source Information</h3>
-            </div>
-            <div>
-              <p className="text-gray-600">Supplier</p>
-              <p>{selectedPalay.palaySupplier.farmerName}</p>
-            </div>
-            <div>
-              <p className="text-gray-600">Farm Origin</p>
-              <p>
-                {selectedPalay.farm.region}, {selectedPalay.farm.province}
-              </p>
-            </div>
-            <div>
-              <p className="text-gray-600">Current Location</p>
-              <p>{selectedPalay.currentlyAt}</p>
-            </div>
-          </div>
-        )}
-      </Dialog>
+        palay={selectedPalay}
+      />
     </StaffLayout>
   );
 }
