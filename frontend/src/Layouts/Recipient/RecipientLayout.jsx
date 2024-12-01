0e409e704bbc77350a9036b9534420dea039ffd9
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2024-12-01 23:40:38 +0800

    resolve import conflict

--- frontend/src/Layouts/Recipient/RecipientLayout.jsx
+++ frontend/src/Layouts/Recipient/RecipientLayout.jsx
@@ -8,7 +8,7 @@ import { Divider } from "primereact/divider";
 
 import Navbar from "./Components/Navbar";
 import { useAuth } from "../../Pages/Authentication/Login/AuthContext";
-import RightSidebar from "../../Components/Recipient/RightSidebar";
+import RightSidebar from "./Components/RightSidebar";
 
 function RecipientLayout({
     children,
