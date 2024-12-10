ca4ba0d8674f7dd35213d151e5886b5a0e12d608
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2024-12-10 16:09:28 +0800

    let's go

--- frontend/src/Pages/App.jsx
+++ frontend/src/Pages/App.jsx
@@ -168,9 +168,9 @@ function App() {
                 <Route
                     path="/staff/*"
                     element={
-                        // <ProtectedRoute allowedUserTypes={["NFA Branch Staff"]}>
-                        // </ProtectedRoute>
+                        <ProtectedRoute allowedUserTypes={["NFA Branch Staff"]}>
                             <StaffRoutes />
+                        </ProtectedRoute>
                     }
                 />
                 <Route
